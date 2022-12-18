1db0157811394891e1f4f1d367331c99f3d8afca
Liping Mechling <65612959+Lime5005@users.noreply.github.com>
2022-12-19 00:12:42 +0100

    Functional app in Azure

---
+++ src/main/java/org/example/functions/Function.java
@@ -0,0 +1,22 @@
+package org.example.functions;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
+
+import java.io.IOException;
+
+
+public class Function {
+
+    @FunctionName("TimerTrigger-Java")
+    public void run(
+            @TimerTrigger(name = "keepAliveTrigger", schedule = "0 30 7 * * *") String timerInfo,
+            final ExecutionContext context) throws IOException {
+
+        String todaysMessage = new DailyMessage().getTodaysMessage();
+        new TwilioSMSSender().sendMessage(todaysMessage);
+
+        return;
+    }
+}
